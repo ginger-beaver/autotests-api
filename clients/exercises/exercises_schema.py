7c75f5410305ff01d48d194f3e49b49763965be1
ginger-beaver <nicky.um@hotmail.com>
2025-09-02 00:32:15 +0300

    Знакомство с Faker. Практика работы с генерацией случайных данных

--- clients/exercises/exercises_schema.py
+++ clients/exercises/exercises_schema.py
@@ -1,4 +1,6 @@
+from pydantic import Field
 from tools.camel_model import CamelModel
+from tools.fake_data_generator import fake
 
 class Exercise(CamelModel):
     """
@@ -39,22 +41,22 @@ class CreateExerciseRequestSchema(CamelModel):
     """
     Описание структуры запроса на создание упражнения.
     """
-    title: str
-    course_id: str
-    max_score: int
-    min_score: int
-    order_index: int
-    description: str
-    estimated_time: str
+    title: str = Field(default_factory=fake.sentence)
+    course_id: str = Field(default_factory=fake.uuid4)
+    max_score: int = Field(default_factory=fake.max_score)
+    min_score: int = Field(default_factory=fake.min_score)
+    order_index: int = Field(default_factory=fake.integer)
+    description: str = Field(default_factory=fake.text)
+    estimated_time: str = Field(default_factory=fake.estimated_time)
 
 
 class UpdateExerciseRequestSchema(CamelModel):
     """
     Описание структуры запроса на обновление упражнения.
     """
-    title: str | None
-    max_score: int | None
-    min_score: int | None
-    order_index: int | None
-    description: str | None
-    estimated_time: str | None
+    title: str | None = Field(default_factory=fake.sentence)
+    max_score: int | None = Field(default_factory=fake.max_score)
+    min_score: int | None = Field(default_factory=fake.min_score)
+    order_index: int | None = Field(default_factory=fake.integer)
+    description: str | None = Field(default_factory=fake.text)
+    estimated_time: str | None = Field(default_factory=fake.estimated_time)
