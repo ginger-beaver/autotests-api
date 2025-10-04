7d70724b805dffa3876d21bfc1c9fe80cbd253ba
ginger-beaver <nicky.um@hotmail.com>
2025-10-04 10:20:38 +0300

    Рефакторинг тестовых классов

--- tests/authentication/test_authentication.py
+++ tests/authentication/test_authentication.py
@@ -12,14 +12,13 @@ from tools.assertions.schema import validate_json_schema
 
 @pytest.mark.regression
 @pytest.mark.authentication
-def test_login(function_user: UserFixture, authentication_client: AuthenticationClient):
-    request = LoginRequestSchema(email=function_user.email, password=function_user.password)
+class TestAuthentication:
+    def test_login(self, function_user: UserFixture, authentication_client: AuthenticationClient):
+        request = LoginRequestSchema(email=function_user.email, password=function_user.password)
+        response = authentication_client.login_api(request)
+        response_data = LoginResponseSchema.model_validate_json(response.text)
 
-    response = authentication_client.login_api(request)
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_login_response(response_data)
 
-    response_data = LoginResponseSchema.model_validate_json(response.text)
-
-    assert_status_code(response.status_code, HTTPStatus.OK)
-    assert_login_response(response_data)
-
-    validate_json_schema(response.json(), response_data.model_json_schema())
+        validate_json_schema(response.json(), response_data.model_json_schema())
