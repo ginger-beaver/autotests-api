c041cb017b89e4ea1ab9bdbb113d8034a8bcdd33
ginger-beaver <nicky.um@hotmail.com>
2025-10-09 17:18:06 +0300

    Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями. Получение задания

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -3,22 +3,44 @@ from http import HTTPStatus
 import pytest
 
 from clients.exercises.exercises_client import ExercisesClient
-from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
+    GetExerciseResponseSchema
 from fixtures.courses import CourseFixture
+from fixtures.exercises import ExerciseFixture
 from tools.assertions.base import assert_status_code
-from tools.assertions.exercises import assert_create_exercise_response
+from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response
 from tools.assertions.schema import validate_json_schema
 
 
 @pytest.mark.exercises
 @pytest.mark.regression
 class TestExercises:
-    def test_create_exercise(self, exercises_client: ExercisesClient, function_course: CourseFixture):
+    def test_create_exercise(
+            self,
+            exercises_client: ExercisesClient,
+            function_course: CourseFixture
+    ):
         request = CreateExerciseRequestSchema(course_id=function_course.response.course.id)
         response = exercises_client.create_exercise_api(request)
         response_data = CreateExerciseResponseSchema.model_validate_json(response.text)
 
         assert_status_code(response.status_code, HTTPStatus.OK)
         assert_create_exercise_response(request, response_data)
+
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_get_exercise(
+            self,
+            exercises_client: ExercisesClient,
+            function_exercise: ExerciseFixture
+    ):
+        response = exercises_client.get_exercise_api(exercise_id=function_exercise.response.exercise.id)
+        response_data = GetExerciseResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_get_exercise_response(
+            get_exercise_response=response_data,
+            create_exercise_response=function_exercise.response
+        )
 
         validate_json_schema(response.json(), response_data.model_json_schema())
--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -1,4 +1,5 @@
-from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
+    ExerciseSchema, GetExerciseResponseSchema
 from tools.assertions.base import assert_equal
 
 
@@ -18,3 +19,38 @@ def assert_create_exercise_response(request: CreateExerciseRequestSchema, respon
             expected=getattr(response.exercise, field_name),
             name=field_name
         )
+
+
+def assert_exercise(actual: ExerciseSchema, expected: ExerciseSchema):
+    """
+    Проверяет, что фактические данные упражнения соответствуют ожидаемым.
+
+    :param actual: Фактические данные упражнения.
+    :param expected: Ожидаемые данные упражнения.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    field_names = ExerciseSchema.model_fields.keys()
+
+    for field_name in field_names:
+        assert_equal(
+            actual=getattr(actual, field_name),
+            expected=getattr(expected, field_name),
+            name=field_name
+        )
+
+
+def assert_get_exercise_response(
+        get_exercise_response: GetExerciseResponseSchema,
+        create_exercise_response: CreateExerciseResponseSchema,
+):
+    """
+    Проверяет, что ответ на получение упражнения соответствует ответу на его создание.
+
+    :param get_exercise_response: Ответ API при запросе упражнения по его id.
+    :param create_exercise_response: Ответ API при создании упражнения.
+    :raises AssertionError: Если данные упражнений не совпадают.
+    """
+    assert_exercise(
+        actual=get_exercise_response.exercise,
+        expected=create_exercise_response.exercise
+    )
